b4261b6b9918c6aeff78935d188521450e553ef2
[SW5] 홍승진 <cmikal47@gmail.com>
2023-08-28 00:19:14 +0900

    Feat : App, index 기본 세팅

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,5 +1,21 @@
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/grobalStyle';
+import { theme } from './styles/theme';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+
+root.render(
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <BrowserRouter basename="">
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </Provider>
+);
